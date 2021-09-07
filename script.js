dad69a3a6045ea2cf6c38bd91e9704b5af7bf962
Adam Tilly <tillyjadam@gmail.com>
2021-09-06 21:23:40 -0500

    fixed mixup bug of lowercase and uppercase being switched and corrected indentation

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ var passwordFunction = {
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", ()=> {
   var length = prompt("length of desired password?");
- lenght =  parseInt(length)
+   lenght =  parseInt(length)
   if(length<8 || length>128){
     window.alert("Please enter a number larger than 8 but smaller than 128")
-   return
+    return
   } 
- console.log(length)
+  console.log(length)
   var hasUpper = window.confirm("Are capitol letters required?");
   var hasLower = window.confirm("Are lower case letters required?");
   var hasSymbol = window.confirm("Are special characters required?");
@@ -25,44 +25,44 @@ generateBtn.addEventListener("click", ()=> {
     window.alert("Please select atleast one character type.")
     return
   }
-   console.log()
- passwordText = generatePassword(hasLower, hasUpper, hasSymbol, hasNumber, length)
- document.getElementById("password").placeholder=passwordText;
+  console.log()
+  passwordText = generatePassword(hasLower, hasUpper, hasSymbol, hasNumber, length)
+  document.getElementById("password").placeholder=passwordText;
 })
 
 
- function generatePassword(upper, lower, symbol, number, length){
-var generatedPassword= '';
-var typesLength = upper + lower + symbol + number;
-console.log('typesLength', typesLength);
+function generatePassword(upper, lower, symbol, number, length){
+  var generatedPassword= '';
+  var typesLength = upper + lower + symbol + number;
+  console.log('typesLength', typesLength);
 
-var typesArray = [{upper},{lower},{symbol},{number}].filter(item => Object.values(item)[0])
-console.log(typesArray)
+  var typesArray = [{upper},{lower},{symbol},{number}].filter(item => Object.values(item)[0])
+  console.log(typesArray)
 
-if(typesLength === 0){
-  return 'Please Try again and select requirments'
+  if(typesLength === 0){
+    return 'Please Try again and select requirments'
+  }
+  console.log(typesLength)
+  console.log(length)
+  for(let i =0; i < length; i+=typesLength){
+
+    typesArray.forEach(type =>{
+    var  passName = Object.keys(type)[0];
+    console.log('passName', passName)
+
+    generatedPassword += passwordFunction[passName]();
+    });
+  }
+  console.log(generatedPassword.slice(0,length))
+  var passwordText = generatedPassword.slice(0,length)
+  return passwordText
 }
-console.log(typesLength)
-console.log(length)
-for(let i =0; i < length; i+=typesLength){
-
-  typesArray.forEach(type =>{
-var  passName = Object.keys(type)[0];
-console.log('passName', passName)
-
-generatedPassword += passwordFunction[passName]();
- });
-}
-console.log(generatedPassword.slice(0,length))
-var passwordText = generatedPassword.slice(0,length)
-return passwordText
- }
 
 
-function getLowerRandom(){
+function getUpperRandom(){
   return String.fromCharCode((Math.floor(Math.random()*26+97)));
 }
-function getUpperRandom(){
+function getLowerRandom(){
   return String.fromCharCode((Math.floor(Math.random()*26+65)));
 }
 function getNumberRandom(){
